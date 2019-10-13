a37ad3df49f560ac4fff1376013196379acb0ac0
Yaromik Evgeni <dom2kb1@yandex.ru>
2019-10-13 13:18:35 +0300

    add auth

---
+++ force-app/main/default/lwc/expenses/expenses.js
@@ -0,0 +1,27 @@
+import { LightningElement, track } from 'lwc';
+
+export default class Expenses extends LightningElement {
+
+    @track login = true;
+    @track employeePage = false;
+    @track adminPage = false;
+
+    show;
+    showPage(){
+        if (this.show === 1){
+            this.login = false;
+            this.employeePage =true;
+            this.adminPage =false;
+        }else if (this.show === 2){
+            this.login = false;
+            this.employeePage =false;
+            this.adminPage =true;            
+        }        
+    }
+
+    hanldeShowValueChange(event) {
+        window.console.log('login = false '+ event.detail);
+        this.show = event.detail;
+        this.showPage();
+    }
+}
---
+++ force-app/main/default/lwc/login/login.js
@@ -0,0 +1,97 @@
+import { LightningElement, track } from 'lwc';
+
+import EXPENSES_LOGO from '@salesforce/resourceUrl/Logo';
+
+// Importing Apex Class method
+import runLoginUser from '@salesforce/apex/ExpensesController.loginUser';
+
+export default class Login extends LightningElement {
+    logoUrl = EXPENSES_LOGO;
+
+    @track error;
+    @track message;
+
+    showPageValue;
+    showPage() {
+        // Creates the event with the data next step.
+        const selectedEvent = new CustomEvent("showvaluechange", {
+            detail: this.showPageValue
+        });
+        // Dispatches the event.
+        this.dispatchEvent(selectedEvent);
+    }
+
+    login;
+    password;
+    //get input login and password
+    setLoginPassword(event) {
+        if (event.target.name === 'login') {
+            this.login = event.target.value;
+            // eslint-disable-next-line no-console
+            console.log('login ' + this.login);
+        }
+        else if (event.target.name === 'password') {
+            this.password = event.target.value;
+            // eslint-disable-next-line no-console
+            console.log('login ' + this.password);
+        }
+    }
+
+    authUser = {};
+    handleLoginUser() {
+        // const allValid = [...this.template.querySelectorAll('lightning-input')]
+        //     .reduce((validSoFar, inputCmp) => {
+        //         inputCmp.reportValidity();
+        //         return validSoFar && inputCmp.checkValidity();
+        //     }, true);
+        // if (!allValid) {
+        //     // eslint-disable-next-line no-alert
+        //     alert('Please update the invalid form entries and try again.');
+        //     return;
+        // }
+        let inputLogin = this.template.querySelector(".login");
+        let inputPassword = this.template.querySelector(".inpPassword");
+        let valueLogin = inputLogin.value;
+        let valuePassword = inputPassword.value;
+        if (valueLogin === '' || valueLogin === undefined) {
+            inputLogin.setCustomValidity("Please enter login");            
+        } else{
+            inputLogin.setCustomValidity("");
+        }
+        inputLogin.reportValidity();
+        if (valuePassword === '' || valuePassword === undefined) {
+            inputPassword.setCustomValidity("Please enter password");
+        } else{
+            inputPassword.setCustomValidity("");
+        }     
+        inputPassword.reportValidity();
+        window.console.log('login= ' + inputLogin.validity.valid);
+        window.console.log('pass= ' + inputPassword.validity.valid);
+        
+        if((!inputLogin.validity.valid) || (!inputPassword.validity.valid)){
+            window.console.log('false = return');
+            return;
+        }
+        runLoginUser({ login: this.login, pass: this.password })
+            .then(result => {
+                if(result && result != null){
+                    window.console.log('result ===> ' + JSON.stringify(result));
+                    this.authUser = result;
+                    if(this.authUser.Admin__c === true){
+                        this.showPageValue = 2;
+                    }else if (this.authUser.Admin__c === false) {
+                        this.showPageValue = 1;
+                    }
+                    this.showPage();
+                }else{
+                    window.console.log('return null ');
+                }               
+                
+                
+            })
+            .catch(error => {
+                this.error = error.message;
+                this.message = 'Login or password incorrect';
+            });
+    }
+}
