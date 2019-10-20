b3d4c0df03af9a65543b485faa46fc01c13c3cfe
Yaromik Evgeni <dom2kb1@yandex.ru>
2019-10-20 20:44:59 +0300

    add selected item

--- force-app/main/default/lwc/employeePage/employeePage.js
+++ force-app/main/default/lwc/employeePage/employeePage.js
@@ -2,6 +2,7 @@ import { LightningElement, track, api } from 'lwc';
 
 import getMouthlyExpense from '@salesforce/apex/ExpensesController.getMouthlyExpense';
 import getExpenseCard from '@salesforce/apex/ExpensesController.getExpenseCard';
+import saveExpenseCard from '@salesforce/apex/ExpensesController.saveExpenseCard';
 
 import { updateRecord } from 'lightning/uiRecordApi';
 //import { refreshApex } from '@salesforce/apex';
@@ -11,16 +12,31 @@ import AMOUNT_FIELD from '@salesforce/schema/ExpenseCard__c.Amount__c';
 import ID_FIELD from '@salesforce/schema/ExpenseCard__c.Id';
 
 const COLS = [
-  { label: 'Description', fieldName: 'Description__c', editable: true },
-  { label: 'Amount', fieldName: 'Amount__c', type: 'phone', editable: true },
-  { label: 'Action', type: 'button', typeAttributes: { variant: 'base', label: 'Delete', name: 'delete', onclick: 'deleteExpenseCard' } }
+  { label: 'Description', fieldName: 'Description__c', fixedWidth: 180, editable: true },
+  { label: 'Amount', fieldName: 'Amount__c', type: 'phone', fixedWidth: 80, editable: true },
+  { label: 'Action', type: 'button', fixedWidth: 80, typeAttributes: { variant: 'base', label: 'Delete', name: 'delete', onclick: 'deleteExpenseCard' } }
 ];
 
 export default class EmployeePage extends LightningElement {
 
+  @track error;
+  @track messageCard;
+
   @api authUser;
 
   @track monthlyExpense;
+
+  @track openmodel = false;
+  @track dateNow;
+  openmodal() {
+    window.console.log('DATE ' + this.dateNow);
+    // this.dateNow = Date.now();
+    this.dateNow = new Date().toISOString();
+    this.openmodel = true;
+  }
+  closeModal() {
+    this.openmodel = false;
+  }
 
   loadMontlyExpense() {
     getMouthlyExpense({ conId: this.authUser.Id })
@@ -39,34 +55,81 @@ export default class EmployeePage extends LightningElement {
     window.console.log('run load ' + this.authUser.Id);
     this.loadMontlyExpense();
     for (let i = -2; i <= 1; i++) {
-      this.dates4years.push(new Date().getFullYear() + i);
+      if (this.selectedYear === new Date().getFullYear() + i) {
+        this.dates4years.push({
+          pItem: new Date().getFullYear() + i,
+          classList: 'slds-button slds-button_brand'
+        });
+      } else {
+        this.dates4years.push({
+          pItem: new Date().getFullYear() + i,
+          classList: 'slds-button slds-button_neutral'
+        });
+      }
     }
-    this.loadDataExpenseCard();
+    // this.loadDataExpenseCard();
   }
 
   dates4years = [];
 
   selectMounth;
   selectedYear = new Date().getFullYear();
+  selectedDate;
 
-  showM(event) {
+  showExpenseCard(event) {
     window.console.log('SelectedYear = ' + this.selectedYear);
-    window.console.log('selectedMounth - ' + event.target.name);
-    this.selectMounth = event.target.name;
+    window.console.log('selectedMounthNOW - ' + event.target.value);
+    this.selectMounth = event.target.value;
+    this.selectedDate = new Date(this.selectMounth);
+    this.selectedDate.setFullYear(this.selectedYear);
+
+    const allTabs = this.template.querySelectorAll('ul>button');
+    allTabs.forEach((elm) => {
+      // eslint-disable-next-line no-console
+      console.log(elm);
+      elm.classList.remove("slds-is-active");
+    })
+    event.currentTarget.classList.add('slds-is-active');
+    this.loadDataExpenseCard();
+  }
+
+  selectYearButton(event) {
+    this.selectedYear = event.target.value;
+    window.console.log('SelectedYearButton = ' + this.selectedYear);
+    const allTabs = this.template.querySelectorAll('div>button');
+    allTabs.forEach((elm) => {
+      // eslint-disable-next-line no-console
+      console.log(elm);
+      elm.classList.remove("slds-button_brand");
+      elm.classList.add("slds-button_neutral");
+    })
+    event.currentTarget.classList.add('slds-button_brand');
   }
 
   @track expensesCard = [];
 
   loadDataExpenseCard() {
-    getExpenseCard({})
+    getExpenseCard({ selectedDate: this.selectedDate })
       .then(result => {
-        //this.expensesCard = result;
-        window.console.log('ExpensesCards=' + JSON.stringify(result));
-        this.expensesCard = [];
-        this.expensesCard = result;
+        if (result) {
+          window.console.log('ExpensesCards=' + JSON.stringify(result));
+          this.expensesCard = [];
+          this.expensesCard = result;
+          this.messageCard = '';
+        } else {
+          this.expensesCard = [];
+          this.messageCard = 'No data for selected month!';
+        }
+
       })
       .catch(error => {
-        this.error = error;
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: 'Error load card',
+            message: error.body.message,
+            variant: 'error'
+          })
+        );
         this.expensesCard = undefined;
       });
   }
@@ -111,18 +174,68 @@ export default class EmployeePage extends LightningElement {
     alert('DELETE??' + event.detail.row.Id);
   }
 
-  handleSum() {
+  newExpenseCard = { 'sobjectType': 'ExpenseCard__c' };
 
-    var el = this.template.querySelector('lightning-datatable');
-    // eslint-disable-next-line no-console
-    console.log('el = ' + el);
-    let selected = el.getSelectedRows();
-    // eslint-disable-next-line no-console
-    console.log('selected = ' + selected);
+  handleSetInputModal(event) {
+    if (event.target.name === 'amount') {
+      this.newExpenseCard.Amount__c = event.target.value;
+    }
+    else if (event.target.name === 'date') {
+      this.newExpenseCard.CardDate__c = event.target.value;
+    }
+    else if (event.target.name === 'description') {
+      this.newExpenseCard.Description__c = event.target.value;
+    }
+  }
 
-
-    let table = this.template.querySelector(".sss");
-
-    window.console.log('SUM ' + table);
+  handleSaveExpenseCard() {
+    var inputAmount = this.template.querySelector(".inputAmount");
+    var value = inputAmount.value;
+    if (value === "") {
+      inputAmount.setCustomValidity("Please enter Amount");
+    } else {
+      inputAmount.setCustomValidity("");
+    }
+    inputAmount.reportValidity();
+    let inputDate = this.template.querySelector(".inputDate");
+    if (inputDate.value === "") {
+      inputDate.setCustomValidity("Please select date");
+    } else {
+      inputDate.setCustomValidity("");
+    }
+    inputDate.reportValidity();
+    let inputDescription = this.template.querySelector(".inputDescription");
+    if (inputDescription.value === undefined || inputDescription.value === " ") {
+      inputDescription.setCustomValidity("Please enter description");
+    } else {
+      inputDescription.setCustomValidity("");
+    }
+    inputDescription.reportValidity();
+    if (!inputAmount.checkValidity() || !inputDate.checkValidity() || !inputDescription.checkValidity()) {
+      return;
+    }
+    if(this.newExpenseCard.CardDate__c === undefined){
+      this.newExpenseCard.CardDate__c = this.dateNow;
+    }
+    saveExpenseCard({ expenseCard: this.newExpenseCard })
+      .then(result => {
+        if (result === "ok") {
+          this.newExpenseCard = {};
+          this.dispatchEvent(new ShowToastEvent({
+            title: 'Success!!',
+            message: 'ExpenseCard Created Successfully!!',
+            variant: 'success'
+          }));
+          this.closeModal();
+        }
+      })
+      .catch(error => {
+        // Show error messsage
+        this.dispatchEvent(new ShowToastEvent({
+          title: 'Error!!',
+          message: error.message,
+          variant: 'error'
+        }));
+      });
   }
 }
